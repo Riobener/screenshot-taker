c98871b14c4aec79e0fb7c4c6ff6d69e71032e5b
Riobener <venisonsheff@gmail.com>
2024-02-17 23:14:46 +0300

    Скриншот полного окна и текущего в фокусе
    Signed-off-by: Riobener <venisonsheff@gmail.com>

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os
 import tkinter as tk
-from datetime import datetime
 import keyboard
-from PIL import ImageTk, ImageGrab, ImageDraw, Image
+import win32gui
+
+from datetime import datetime
+from PIL import ImageTk, ImageGrab, Image
 from pystray import Icon as icon, Menu as menu, MenuItem as item
+from win32gui import GetForegroundWindow
 
 WIDTH, HEIGHT = 1920, 1080
 
 
-class ScreenTaker():
+class ScreenTaker:
     def __init__(self):
         self.screenshot = ImageGrab.grab()
         self.window = tk.Tk()
@@ -19,11 +22,10 @@ class ScreenTaker():
         self.window.configure(background='grey')
         self.img = ImageTk.PhotoImage(self.screenshot)
         self.canvas = tk.Canvas()
-        self.screenshot.close()
         self.topx, self.topy, self.botx, self.boty = 0, 0, 0, 0
         self.rect_id = None
 
-    def window_logic(self):
+    def take_with_area(self):
         self.canvas = tk.Canvas(self.window, width=self.img.width(), height=self.img.height(),
                                 borderwidth=0, highlightthickness=0)
         self.canvas.pack(expand=True)
@@ -52,24 +54,22 @@ class ScreenTaker():
 
     def release_and_save(self, event):
         self.window.destroy()
-        screenshot = ImageGrab.grab(bbox=self.convert_coords())
-        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-        filename = f"screenshot_{current_time}.png"
-        save_path = os.path.join(desktop_path, filename)
-        screenshot.save(save_path)
+        self.take_by_coords(self.convert_coords())
+
+    def take_by_coords(self, coords):
+        self.screenshot = ImageGrab.grab(bbox=coords)
+        self.screenshot.save(get_save_path())
+        self.screenshot.close()
+
+    def take_fullscreen(self):
+        self.screenshot.save(get_save_path())
+        self.screenshot.close()
 
 
-
-def on_hotkey():
-    taker = ScreenTaker()
-    taker.window_logic()
-
-
-class ProgramWorker():
+class PyStrayWorker:
     def __init__(self):
         self.icon = icon('test', Image.open("icon.png"), menu=menu(
-            item(text="Left-Click-Action", action=on_hotkey, default=True),
+            item(text="Left-Click-Action", action=on_selecting_area, default=True),
             item(
                 'Exit',
                 self.end_program))).run()
@@ -79,10 +79,40 @@ class ProgramWorker():
         exit(1)
 
 
+def get_save_path():
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+    filename = f"screenshot_{current_time}.png"
+    save_path = os.path.join(desktop_path, filename)
+    return save_path
+
+
+def on_selecting_area():
+    ScreenTaker().take_with_area()
+
+
+def on_fullscreen():
+    ScreenTaker().take_fullscreen()
+
+
+def on_foreground_window():
+    window_coords = list(win32gui.GetWindowRect(GetForegroundWindow()))
+    print(window_coords)
+    window_coords[0] = window_coords[0] + 8
+    window_coords[2] = window_coords[2] - 8
+    window_coords[1] = window_coords[1] + 8
+    window_coords[3] = window_coords[3] - 10
+    ScreenTaker().take_by_coords(window_coords)
+
+
 def main():
-    print("Hotkey 'Ctrl+F12' set to show fullscreen screenshot with the ability to draw a rectangle area")
-    keyboard.add_hotkey('ctrl+f12', on_hotkey)
-    ProgramWorker()
+    print("Hotkey 'Ctrl+F12' set to take a screenshot with area selecting tool")
+    print("Hotkey 'Ctrl+F11' set to take a screenshot of full screen")
+    print("Hotkey 'Ctrl+F10' set to take a screenshot of foreground window")
+    keyboard.add_hotkey('ctrl+f12', on_selecting_area)
+    keyboard.add_hotkey('ctrl+f11', on_fullscreen)
+    keyboard.add_hotkey('ctrl+f10', on_foreground_window)
+    PyStrayWorker()
 
 
 if __name__ == "__main__":
